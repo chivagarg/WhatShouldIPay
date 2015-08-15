a4abc54ce83c597b09a025fc59dab953a5b26723
chivagarg <user@ShivaG-MacBook-Pro.PK5001Z>
2015-08-15 14:23:10 -0700

    Hook up pipeline, add mock plotly, convert to single page app

--- app.js
+++ app.js
@@ -28,23 +28,136 @@ server.listen(app.get('port'), function() {
 	console.log("Utility Percentiles running on port " + app.get('port'));
 })
 
+// var trace1 = {
+//   x: [1, 2, 3, 4, 5],
+//   y: [10, 15, 13, 17, 22],
+//   type: "scatter"
+// };
+// var trace2 = {
+//   x: [1, 2, 3, 4],
+//   y: [16, 5, 11, 9],
+//   type: "scatter"
+// };
+// var data = [trace1, trace2];
+// var graphOptions = {filename: "privacy-false", world_readable: true, fileopt: "overwrite"};
+// plotly.plot(data, graphOptions, function (err, msg) {
+//     console.log(msg);
+// });
+var arrx = [19097, 18601, 15595, 13546, 12026, 7434, 5419];
+var arry = [43, 47, 56, 80, 86, 93, 80];
+
 var trace1 = {
-  x: [1, 2, 3, 4, 5],
-  y: [10, 15, 13, 17, 22],
+  x: [52698, 43117],
+  y: [53, 31],
+  mode: "markers",
+  name: "North America",
+  text: ["United States", "Canada"],
+  marker: {
+    color: "rgb(164, 194, 244)",
+    size: 12,
+    line: {
+      color: "white",
+      width: 0.5
+    }
+  },
   type: "scatter"
 };
 var trace2 = {
-  x: [1, 2, 3, 4],
-  y: [16, 5, 11, 9],
+  x: [39317, 37236, 35650, 30066, 29570, 27159, 23557, 21046, 18007],
+  y: [33, 20, 13, 19, 27, 19, 49, 44, 38],
+  mode: "markers",
+  name: "Europe",
+  text: ["Germany", "Britain", "France", "Spain", "Italy", "Czech Rep.", "Greece", "Poland"],
+  marker: {
+    color: "rgb(255, 217, 102)",
+    size: 12,
+    line: {
+      color: "white",
+      width: 0.5
+    }
+  },
   type: "scatter"
 };
-var data = [trace1, trace2];
-var graphOptions = {filename: "privacy-false", world_readable: true, fileopt: "overwrite"};
+var trace3 = {
+  x: [42952, 37037, 33106, 17478, 9813, 5253, 4692, 3899],
+  y: [23, 42, 54, 89, 14, 99, 93, 70],
+  mode: "markers",
+  name: "Asia/Pacific",
+  text: ["Australia", "Japan", "South Korea", "Malaysia", "China", "Indonesia", "Philippines", "India"],
+  marker: {
+    color: "rgb(234, 153, 153)",
+    size: 12,
+    line: {
+      color: "white",
+      width: 0.5
+    }
+  },
+  type: "scatter"
+};
+var trace4 = {
+  x: arrx,
+  y: arry,
+  mode: "markers",
+  name: "Latin America",
+  //text: ["Chile", "Argentina", "Mexico", "Venezuela", "Venezuela", "El Salvador", "Bolivia"],
+  marker: {
+    color: "rgb(142, 124, 195)",
+    size: 12,
+    line: {
+      color: "white",
+      width: 0.5
+    }
+  },
+  type: "scatter"
+};
+var data = [trace1, trace2, trace3, trace4];
+var layout = {
+  title: "Quarter 1 Growth",
+  xaxis: {
+    title: "GDP per Capita",
+    showgrid: false,
+    zeroline: false
+  },
+  yaxis: {
+    title: "Percent",
+    showline: false
+  }
+};
+var graphOptions = {layout: layout, filename: "line-style", fileopt: "overwrite"};
 plotly.plot(data, graphOptions, function (err, msg) {
     console.log(msg);
 });
 
 
-require(path.join(__dirname, 'routes/routes.js'))(express, app, bodyParser, mongoose);
+// define the Model outside of the routes. 
+// Todo - export this
+var UtilityRent = mongoose.model('UtilityRent', { zipcode: Number, bedrooms: Number, bathrooms: Number, rental: Number });
+
+
+//var Potato = mongoose.model('Potato', PotatoSchema);
+
+// var potatoBag = [{ zipcode: 98103, bedrooms: 2, bathrooms: 1.5, rental: 1995 },
+//                 { zipcode: 98103, bedrooms: 2, bathrooms: 1, rental: 2225 },
+//                 { zipcode: 98103, bedrooms: 1, bathrooms: 1, rental: 1575 },
+//                 { zipcode: 98103, bedrooms: 1, bathrooms: 1, rental: 1795 },
+//                 { zipcode: 98103, bedrooms: 2, bathrooms: 1, rental: 1550 },
+//                 { zipcode: 98103, bedrooms: 4, bathrooms: 3, rental: 3950 },
+//                 { zipcode: 98103, bedrooms: 3, bathrooms: 2, rental: 3000 },
+//                 { zipcode: 98103, bedrooms: 2, bathrooms: 1.5, rental: 1980 },
+//                 { zipcode: 98103, bedrooms: 3, bathrooms: 3, rental: 2500 }
+//                 ];
+
+// UtilityRent.collection.insert(potatoBag, onInsert);
+
+
+function onInsert(err, docs) {
+    if (err) {
+        // TODO: handle error
+    } else {
+        console.info('%d potatoes were successfully stored.', docs.length);
+    }
+}
+
+require(path.join(__dirname, 'routes/routes.js'))(express, app, bodyParser, UtilityRent);
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(express, app, bodyParser, mongoose){
+module.exports = function(express, app, bodyParser, UtilityRent){
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -7,31 +7,79 @@ router.get('/', function(req, res, next){
 	res.render('index', {});
 })
 
-router.get('/visualize', function(req, res, next){
-	res.render('visualize', {});
-})
+// router.get('/visualizeget', function(req, res, next){
+// 	console.log("Recived!!!");
+// 	res.render('visualize', {title: 'visualizing data for you!'});
+// })
+
+// router.get('/upload', function(req, res, next){
+// 	console.log("Strigified answers - " + JSON.stringify(req.query));
+
+// 	var context = JSON.stringify(req.query.context);
+// 	console.log("Context is " + context);
+// 	//console.log("zipcode is " + req.query.zipcode);
+// 	var zipcode = parseInt(req.query.zipcode);
+// 	var bedrooms = parseInt(req.query.bedrooms);
+// 	var bathrooms = parseInt(req.query.bathrooms);
+// 	var rent = parseInt(req.query.rent);
+
+// 	res.send({title: 'Hey there delilah'});
+
+// })
 
 router.post('/upload', urlencodedParser, function(req, res, next){
 
 console.log("Recived post from client!" + "req.body is " + req.body.length + JSON.stringify(req.params) + " and " + JSON.stringify(req.body));
 
 var context = JSON.stringify(req.body.context);
-var zipcode = parseInt(stripAlphaChars(JSON.stringify(req.body.zipcode)));
-var bedrooms = parseInt(stripAlphaChars(JSON.stringify(req.body.bedrooms)));
-var bathrooms = parseInt(stripAlphaChars(JSON.stringify(req.body.bathrooms)));
-var rent = parseInt(stripAlphaChars(JSON.stringify(req.body.rent)));
-
-var UtilityRent = mongoose.model('UtilityRent', { zipcode: Number, bedrooms: Number, bathrooms: Number, rental: Number });
+var zipcode = parseInt(stripAlphaChars(req.body.zipcode));
+var bedrooms = parseInt(stripAlphaChars(req.body.bedrooms));
+var bathrooms = parseInt(stripAlphaChars(req.body.bathrooms));
+var rent = parseInt(stripAlphaChars(req.body.rent));
 
 var document = new UtilityRent({zipcode: zipcode, bedrooms: bedrooms, bathrooms: bathrooms, rental: rent});
-document.save(function(err){
-	if (err)
-		console.log(err);
-	else
-		console.log("Saved to DB yo!");
+// document.save(function(err){
+// 	if (err)
+// 		console.log(err);
+// 	else
+// 		console.log("Saved to DB yo!");
+// });
+
+console.log("Going to send response now. Name is john hehe");
+
+
+var meanRent = 0;
+var results = UtilityRent.find({zipcode:zipcode, bedrooms:bedrooms, bathrooms:bathrooms}, function(err, items){
+  console.log("Number of items retrieved - " + items.length);
+  var sum = 0;
+  for (var i=0; i<items.length; ++i)
+  {
+    if (items[i].rental)
+      sum+=items[i].rental;
+  }
+  meanRent = sum/items.length;
+  console.log("Sum is " + sum + " and mean rent is " + meanRent);
+
+  var resObj = {};
+  resObj['user'] = meanRent;
+
+  var avgRent = sum/items.length;
+  res.json({
+  			 userRent: rent,
+  			 avgRent: avgRent
+  		   }
+  		  );
+
 });
 
-res.send({redirect: '/visualize'});
+console.log("Val of mean rent outside " + meanRent);
+
+//res.json(JSON.stringify(resObj));
+
+//var obj = {"tid":meanRent};
+//res.send(JSON.stringify(obj));
+//res.send({tid : 20});
+//res.json({test:'abc'});
 
 })
 
